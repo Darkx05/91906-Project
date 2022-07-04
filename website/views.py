d8c82e0d33c208ad74dcc965610fd21189607902
iVTXiC3rZD1rKiVTXi  (死) <78504630+Darkx05@users.noreply.github.com>
2022-07-05 07:43:13 +1200

    Add files via upload

--- website/views.py
+++ website/views.py
@@ -1,1 +1,15 @@
+# this is the file where all routes are made
+from flask import Blueprint, render_template
 
+views = Blueprint('views', __name__)
+
+# route is for home
+
+
+@views.route('/')
+def base():
+    return render_template("base.html")
+
+@views.route('/')
+def home():
+    return render_template("home.html")
