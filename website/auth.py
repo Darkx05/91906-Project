62a8b72433e1057bf273acd8f783dd0f698b496a
iVTXiC3rZD1rKiVTXi  (死) <78504630+Darkx05@users.noreply.github.com>
2022-07-05 10:15:34 +1200

    Update auth.py
    This commit is to show the user handling for post requests (rules are set to improve validity of user inputs)

--- website/auth.py
+++ website/auth.py
@@ -1,5 +1,5 @@
-from flask import Blueprint, render_template
-from templates import base
+from flask import Blueprint, render_template, request
+
 auth = Blueprint('auth', __name__)
 
 # routes/pathways
@@ -7,8 +7,10 @@ auth = Blueprint('auth', __name__)
 # if users are to move manually
 
 
-@auth.route('/login')
+@auth.route('/login', method=['GET, POST'])
 def login_page():
+    data = request.form()
+    print(data)
     return render_template("login.html", boolean=True)
 
 
@@ -17,7 +19,24 @@ def logout():
     return "<p>logout</p>"
 
 
-@auth.route('/sign-up')
+@auth.route('/sign-up', method=['GET, POST'])
 def signup():
-    return render_template("sign_up.html")
+    # user handling on post requests
+    if request.method == 'POST':
+        email = request.form.get('email')
+        firstName = request.form.get('firstName')
+        password1 = request.form.get('password1')
+        password2 = request.form.get('password2')
 
+        if len(email) < 4:
+            pass
+        elif len(firstName) < 2:
+            pass
+        elif password1 != password2:
+            pass
+        elif len(password1) < 7:
+            pass
+        else:
+            # adds user info to database
+            pass
+    return render_template("sign_up.html")
