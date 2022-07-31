7b4ad22c3b1bd9a47649b22e148f00d212d09c92
iVTXiC3rZD1rKiVTXi  (死) <78504630+Darkx05@users.noreply.github.com>
2022-07-31 17:34:01 +1200

    Update auth.py

--- website/auth.py
+++ website/auth.py
@@ -1,75 +1,74 @@
-# all imports are here
+# imports
 from flask import Blueprint, render_template, request, flash, redirect, url_for
 from .models import User
 from werkzeug.security import generate_password_hash, check_password_hash
 from . import db
 from flask_login import login_user, login_required, logout_user, current_user
 
+
 auth = Blueprint('auth', __name__)
 
-# routes/pathways
-# brackets after auth route is required to move around the site
-# if users are to move manually
-
-
+# auth login page route
 @auth.route('/login', methods=['GET', 'POST'])
-def login_page():
-    if request.method =='POST':
+def login():
+    if request.method == 'POST':
         email = request.form.get('email')
         password = request.form.get('password')
 
         user = User.query.filter_by(email=email).first()
         if user:
             if check_password_hash(user.password, password):
-                flash('Login Succsessful', category='success')
+                flash('Logged in successfully!', category='success')
                 login_user(user, remember=True)
                 return redirect(url_for('views.home'))
             else:
-                flash('wrong password, try again', category='error')
+                flash('Incorrect password, try again.', category='error')
         else:
             flash('Email does not exist.', category='error')
 
-    return render_template("login.html", boolean=True)
+    return render_template("login.html", user=current_user)
 
 
 @auth.route('/logout')
 @login_required
 def logout():
-    logout_user
+    logout_user()
     return redirect(url_for('auth.login'))
 
 
 @auth.route('/sign-up', methods=['GET', 'POST'])
-def signup():
-    # user handling on post requests
+def sign_up():
     if request.method == 'POST':
+        #variables required 
         email = request.form.get('email')
-        firstName = request.form.get('firstName')
+        first_name = request.form.get('firstName')
         password1 = request.form.get('password1')
         password2 = request.form.get('password2')
-        
+
         user = User.query.filter_by(email=email).first()
+        # if user tries to sign up when an account is existing
+        # this message appears
         if user:
             flash('Email already exists.', category='error')
-        if len(email) < 4:
-            flash('Sorry, that is invalid!', category='try again')
-        elif len(firstName) < 2:
-            flash('Sorry, that is invalid!', category='try again')
+        # exception handling in form
+        elif len(email) < 4:
+            flash('Email must be greater than 3 characters.', category='error')
+        elif len(first_name) < 2:
+            flash('First name must be greater than 1 character.', category='error')
         elif password1 != password2:
-            flash('Passwords don\'t match!', category='try again')
+            flash('Passwords don\'t match.', category='error')
         elif len(password1) < 7:
-            flash('Sorry, that is invalid', category='try again')
+            flash('Password must be at least 7 characters.', category='error')
         else:
-            # this part creates the account
-            new_user = User(email=email, firstName=firstName, password=generate_password_hash(
-                password1, method='sha256'))
+            new_user = User(email=email, first_name=first_name, 
+            password=generate_password_hash(password1, method='sha256'))
             db.session.add(new_user)
             db.session.commit()
-            login_user(user, remember=True)
-            # adds user info to database
-            flash('Details saved', category='Success!')
+            login_user(new_user, remember=True)
+            flash('Account created!', category='success')
             return redirect(url_for('views.home'))
-    return render_template("sign_up.html")
+
+    return render_template("sign_up.html", user=current_user)
 
 # route for the about page
 @auth.route('/about')
