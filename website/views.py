9d710222919830a7a61c8409b024f0a539ff9c39
iVTXiC3rZD1rKiVTXi  (死) <78504630+Darkx05@users.noreply.github.com>
2022-07-31 16:27:40 +1200

    note box addition with exception handling.

--- website/views.py
+++ website/views.py
@@ -1,12 +1,42 @@
-# this is the file where all routes are made
-from flask import Blueprint, render_template
+# imports
+from flask import Blueprint, render_template, request, flash, jsonify
 from flask_login import login_required, current_user
+from .models import Note
+from . import db
+import json
 
 views = Blueprint('views', __name__)
 
-# this single route is for the home page
+# the route below allows the note box
+# to actually record text.
+@views.route('/', methods=['GET', 'POST'])
+@login_required
+def home():
+    if request.method == 'POST':
+        note = request.form.get('note')
+        # exception handling that text must be
+        # entered in order for notes to work
+        if len(note) < 1:
+            flash('Note is too short!', category='error')
+        else:
+            new_note = Note(data=note, user_id=current_user.id)
+            db.session.add(new_note)
+            db.session.commit()
+            # message shown when a note is added
+            flash('Note added!', category='success')
 
+    return render_template("home.html", user=current_user)
 
-@views.route('/')
-def homepage():
-    return render_template("home.html")
+# this route is for when notes
+# are removed
+@views.route('/delete-note', methods=['POST'])
+def delete_note():
+    note = json.loads(request.data)
+    noteId = note['noteId']
+    note = Note.query.get(noteId)
+    if note:
+        if note.user_id == current_user.id:
+            db.session.delete(note)
+            db.session.commit()
+
+    return jsonify({})
