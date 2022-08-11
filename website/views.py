92caf8361552eb189b2291dbe99cb2c55f706a29
iVTXi C3rZDZ iVTXi  (死) <78504630+Darkx05@users.noreply.github.com>
2022-08-12 08:51:50 +1200

    Update views.py

--- website/views.py
+++ website/views.py
@@ -11,36 +11,7 @@ views = Blueprint('views', __name__)
 # to actually record text.
 
 
-@views.route('/', methods=['GET', 'POST'])
+@views.route('/')
 @login_required
 def home():
-    if request.method == 'POST':
-        note = request.form.get('note')
-        # exception handling that text must be
-        # entered in order for notes to work
-        if len(note) < 1:
-            flash('You put nothing! try again!', category='error')
-        else:
-            new_note = Note(data=note, user_id=current_user.id)
-            db.session.add(new_note)
-            db.session.commit()
-            # message shown when a note is added
-            flash('Note added!', category='success')
-
     return render_template("home.html", user=current_user)
-
-# this route is for when notes
-# are removed
-
-
-@views.route('/delete-note', methods=['POST'])
-def delete_note():
-    note = json.loads(request.data)
-    noteId = note['noteId']
-    note = Note.query.get(noteId)
-    if note:
-        if note.user_id == current_user.id:
-            db.session.delete(note)
-            db.session.commit()
-
-    return jsonify({})
