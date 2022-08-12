e8acf4838db3b13b5a4b3321fde9c73fa523ee1d
iVTXi C3rZDZ iVTXi  (死) <78504630+Darkx05@users.noreply.github.com>
2022-08-12 18:08:24 +1200

    Update views.py

--- website/views.py
+++ website/views.py
@@ -7,11 +7,12 @@ import json
 
 views = Blueprint('views', __name__)
 
-# the route below allows the note box
-# to actually record text.
+# the route below allows home to run
 
 
 @views.route('/')
+# this is for the home page as it will
+# show only when logged in
 @login_required
 def home():
     return render_template("home.html", user=current_user)
